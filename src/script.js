c11d947fc47f091a0fac7e8b9892b507d2907995
nk-on <2nkoz@proton.me>
2024-08-04 17:04:14 -0700

    Choosing variables

---
+++ src/script.js
@@ -0,0 +1,3 @@
+const rollButton = document.querySelector("[data-roll]");
+const container = document.querySelector("[data-container]");
+const diceIcon = document.querySelector("[data-dice-icon]");
