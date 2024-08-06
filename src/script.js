eb5c83b75df55778e89e3ab9796e96b3d5c0f56b
nk-on <2nkoz@proton.me>
2024-08-05 22:05:45 -0700

    Feat:implementing function which will generate random number from 1 to 6 and relevant diceicon

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
 const rollButton = document.querySelector("[data-roll]");
 const container = document.querySelector("[data-container]");
 const diceIcon = document.querySelector("[data-dice-icon]");
+function genereteRandomNum() {
+  const diceIcons = {
+    1: "⚀",
+    2: "⚁",
+    3: "⚂",
+    4: "⚃",
+    5: "⚄",
+    6: "⚄",
+  };
+  const [min,max] = [1,6];
+  const randomNum = Math.floor(Math.random()*(max-min)+min);
+  return diceIcons[randomNum];
+};
+//App should be able to generete 6 random numbers
+//there should be objects with numbers and according dice icons
