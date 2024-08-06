0c4447ff434e22edf5066e8aa215edbdfac03c62
nk-on <2nkoz@proton.me>
2024-08-06 08:08:11 -0700

    Feat:adding track history of dice roll results

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const rollButton = document.querySelector("[data-roll]");
 const container = document.querySelector("[data-container]");
 const diceIcon = document.querySelector("[data-dice-icon]");
+let totalRolls = 0;
 function genereteRandomNum() {
   const diceIcons = {
     1: "⚀",
@@ -14,10 +15,24 @@ function genereteRandomNum() {
   const randomNum = Math.floor(Math.random() * (max - min) + min);
   return diceIcons[randomNum];
 }
+function trackHistory(diceIcon) {
+  const template = document.getElementsByTagName("template")[0];
+  const clone = template.cloneNode(true);
+  const resultContainer =  clone.content.querySelector(
+    ".dice-roll-result-container"
+  );
+  const title = resultContainer.querySelector("[data-title]");
+  const diceContainer = resultContainer.querySelector("[ data-dice-container]");
+  title.textContent = totalRolls;
+  diceContainer.textContent = diceIcon;
+  container.appendChild(resultContainer);
+}
 function addDiceIcon() {
   const dice = genereteRandomNum();
   diceIcon.textContent = dice;
-  diceIcon.classList.add('roll-animation');
+  diceIcon.classList.add("roll-animation");
+  totalRolls++;
+  trackHistory(dice);
 }
 rollButton.addEventListener("click", addDiceIcon);
 //App should be able to generete 6 random numbers
