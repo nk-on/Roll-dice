4384b900514865ded3d4995488478d5b6d6795c7
nk-on <2nkoz@proton.me>
2024-08-10 22:35:18 -0700

    Adding roll dice results to container

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const rollButton = document.querySelector("[data-roll]");
 const container = document.querySelector("[data-container]");
 const diceIcon = document.querySelector("[data-dice-icon]");
 let totalRolls = 0;
-const rollHistory = JSON.parse(localStorage.getItem('rollResult')) || [];
+const rollHistory = JSON.parse(localStorage.getItem("rollResult")) || [];
 function genereteRandomNum() {
   const diceIcons = {
     1: "⚀",
@@ -18,13 +18,12 @@ function genereteRandomNum() {
 }
 function historyLocalStorage(diceIcon) {
   rollHistory.push({ diceIcon, totalRolls });
-  localStorage.setItem('rollResult', JSON.stringify(rollHistory));
+  localStorage.setItem("rollResult", JSON.stringify(rollHistory));
 }
 function displayHistory() {
-  if(container.innerHTML.length > 1){
-    container.innerHTML = '';
-  };
-  console.log(rollHistory)
+  if (container.innerHTML.length > 1) {
+    container.innerHTML = "";
+  }
   rollHistory.forEach((rollResult) => {
     const template = document.getElementsByTagName("template")[0];
     const clone = template.cloneNode(true);
@@ -47,9 +46,8 @@ function addDiceIcon() {
   totalRolls++;
   historyLocalStorage(dice);
   displayHistory();
-};
+}
 rollButton.addEventListener("click", addDiceIcon);
-console.log(rollHistory)
 displayHistory();
 //App should be able to generete 6 random numbers
 //there should be objects with numbers and according dice icons
